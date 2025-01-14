ba07684fc3f577fcfcb2687c4e743f94efe8b0c6
andre-felipe-wonsik-alves <andrefwalves@gmail.com>
2025-01-14 16:08:48 -0300

    update formacao

--- src/classes/Formacao.ts
+++ src/classes/Formacao.ts
@@ -1,13 +1,38 @@
+export class Formacao {
+  constructor(
+    private id,
+    private nome,
+    private cargaHoraria,
+    private maxParticipantes
+  ) {}
 
-export class Formacao{
-    constructor(
-        private id,
-        private nome,
-        private cargaHoraria,
-        private maxParticipantes,
-    ){}
+  public getId(): number {
+    return this.id;
+  }
+  public getNome(): string {
+    return this.nome;
+  }
+  public getCargaHoraria(): number {
+    return this.cargaHoraria;
+  }
+  public getMaxParticipantes(): number {
+    return this.maxParticipantes;
+  }
 
-    desatrelarFormacao(){
-        
-    }
+  public setId(id: number) {
+    this.id = id;
+  }
+  public setNome(nome: string) {
+    this.nome = nome;
+  }
+  public setCargaHoraria(cargaHoraria: number) {
+    this.cargaHoraria = cargaHoraria;
+  }
+  public setMaxParticipantes(maxParticipantes: number) {
+    this.maxParticipantes = maxParticipantes;
+  }
+
+  existeFormacaoSei() {}
+  apagarFormacaoSei() {}
+  apagarFormacao() {}
 }
