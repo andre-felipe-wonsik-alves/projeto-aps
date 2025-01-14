a05938e7548d50d84db2b7a7d30356e8f5e017fd
MarvelBR <113470770+MarvelBR@users.noreply.github.com>
2025-01-14 17:10:10 -0300

    Adição da Inscrição

--- src/classes/Participante.ts
+++ src/classes/Participante.ts
@@ -1,22 +1,24 @@
 const Professor = require("./Professor")
 
+//import { Professor } from "./Professor";
+
 export class Participante extends Professor{
     private idParticipante: number;
 
     // método construtor
     // import { ViewService } from "./view.service";
     // public viewService: ViewService,
-    constructor(matriculaSiape: number, nome: string, cpf: string, dataNascimento: Date, idProfessor: number, idParticipante: number) {
-        super(matriculaSiape, nome, cpf, dataNascimento, idProfessor);
+    constructor(matriculaSiape: number, cpf: string, nome: string, dataNascimento: Date, idProfessor: number, idParticipante: number) {
+        super(matriculaSiape, cpf, nome, dataNascimento, idProfessor);
         //this.idProfessor = Professor.getIdProfessor();
         this.idParticipante = idParticipante;
     }
 
-    validarCargaHoraria(){
-        console.log(`{this.nome} teve sua carga validada!`)
-    }
-
-    getNomeParticipante(): number{
+    getIdParticipante(): number{
         return this.idParticipante;
     }
+
 }
+
+//const Participante1 = new Participante(Professor.getMatriculaSiape(), Professor.getCpf(), Professor.getNome(), Professor.getDataNascimento(), Professor.getIdProfessor(), 69);
+//export default Participante1;
--- src/classes/Professor.ts
+++ src/classes/Professor.ts
@@ -1,12 +1,14 @@
 const ServidorUTFPR = require("./ServidorUtfpr")
 
+//import { ServidorUTFPR } from "./ServidorUtfpr";
+
 export class Professor extends ServidorUTFPR {
     // definição dos atributos
     private idProfessor: number;
 
     // método construtor
-    constructor(matriculaSiape: number, nome: string, cpf: string, dataNascimento: Date, idProfessor: number) {
-        super(matriculaSiape, nome, cpf, dataNascimento);
+    constructor(matriculaSiape: number, cpf: string, nome: string, dataNascimento: Date, idProfessor: number) {
+        super(matriculaSiape, cpf, nome, dataNascimento);
         this.idProfessor = idProfessor;
     }
 
@@ -14,11 +16,11 @@ export class Professor extends ServidorUTFPR {
         return this.idProfessor;
     }
 
-    getServidorUTFPR() {
+    getServidorUTFPR() { 
         return `MatriculaSIAPE: ${super.getMatriculaSiape()}, CPF: ${super.getCpf()},
         Nome: ${super.getNome()}, Data de Nascimento: ${super.getDataNascimento()}`;
     }
 }
 
-const Professor1 = new Professor(ServidorUTFPR.getMatriculaSiape(), ServidorUTFPR.getCpf(), ServidorUTFPR.getNome(), 
-ServidorUTFPR.getDataNascimento(), 3678);
+//const Professor1 = new Professor(ServidorUTFPR.getMatriculaSiape(), ServidorUTFPR.getCpf(), ServidorUTFPR.getNome(), 
+//ServidorUTFPR.getDataNascimento(), 3678);
