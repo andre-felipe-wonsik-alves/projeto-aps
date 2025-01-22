eb7e81be9da8b2a6eaed3fe97e4cd8bee1d9c3e0
MarvelBR <113470770+MarvelBR@users.noreply.github.com>
2025-01-22 14:19:09 -0300

    a

--- src/DAOs/DAOParticipante.ts
+++ src/DAOs/DAOParticipante.ts
@@ -1,9 +1,17 @@
+<<<<<<< Updated upstream
 import { DAOGenerico } from './DAOGenerico'; // Importando o DAOGenérico
 import { Participante } from '../classes/Participante'; // Importando a classe Participante
 
 // Criando a classe DAOParticipante que usa o DAOGenerico com Participante
 export class DAOParticipante extends DAOGenerico<Participante> {
 
+=======
+import { DaoGenerico } from "./DaoGenerico"; // Importando o DAOGenérico
+import { Participante } from "../classes/Participante"; // Importando a classe Participante
+
+// Criando a classe DAOParticipante que usa o DAOGenerico com Participante
+export class DAOParticipante extends DaoGenerico<Participante> {
+>>>>>>> Stashed changes
   // Método específico para encontrar o Participante pelo idParticipante
   findByIdParticipante(idParticipante: number): Participante | null {
     return this.dados.find(item => item.getIdParticipante() === idParticipante) || null;
