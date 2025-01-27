f91b459b9c8970a9289145c749e217c68488aa5f
enerzilius <efblouback@gmail.com>
2025-01-27 20:52:29 -0300

    adequação ao DaoManager + refatoração de nomes

--- src/DAOs/DAOParticipante.ts
+++ src/DAOs/DAOParticipante.ts
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import { DaoGenerico } from "./DaoGenerico"; // Importando o DAOGenérico
-import { Participante } from "../classes/Participante"; // Importando a classe Participante
-
-// Criando a classe DAOParticipante que usa o DAOGenerico com Participante
-export class DAOParticipante extends DaoGenerico<Participante> {
-=======
 import { DaoGenerico } from './DaoGenerico'; // Importando o DAOGenérico
 import { Participante } from '../classes/Participante'; // Importando a classe Participante
 
 // Criando a classe DAOParticipante que usa o DAOGenerico com Participante
 export class DaoParticipante extends DaoGenerico<Participante> {
   
->>>>>>> Sigma
   // Método específico para encontrar o Participante pelo idParticipante
   public encontrarPorIdParticipante(idParticipante: number): Participante | null {
     return (this.dados.find((item) => item.getIdParticipante() === idParticipante) || null);
--- src/implementacoes/espelharFormacao/index.ts
+++ src/implementacoes/espelharFormacao/index.ts
@@ -1,12 +1,13 @@
 import { Formacao } from "../../classes/Formacao";
-import { DaoFormacao } from "../../DAOs/DaoFormacao";
+import { DaoManager } from "../../managers/DaoManager";
 import { DvoFormacao } from "../../DVOs/DvoFormacao";
-import { ManagerFormacao } from "../../managers/managerFormacao";
+import { FormacaoManager } from "../../managers/FormacaoManager";
 
 export async function main() {
-  const daoFormacao = new DaoFormacao();
+  const man = new DaoManager;
+  const daoFormacao =  man.getDaoFormacao();
   const dvoFormacao = new DvoFormacao(daoFormacao);
-  const formacaoManager = new ManagerFormacao(daoFormacao, dvoFormacao);
+  const formacaoManager = new FormacaoManager(daoFormacao, dvoFormacao);
 
   const novaFormacao = new Formacao(1, "eberzao", 100, 2);
 
@@ -16,7 +17,7 @@ export async function main() {
         "Sigma"
     );
 
-    console.log(retorno);
+    
   } catch (error) {
     console.error("Erro:", error.message);
   }
