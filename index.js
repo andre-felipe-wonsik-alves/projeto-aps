06f1a2c330e1b85ca35a8ddba7650c658128e378
andre-felipe-wonsik-alves <aalves.2020@alunos.utfpr.edu.br>
2025-02-21 14:19:14 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import { main } from "./src/implementacoes/desatrelarFormacao/MockIndex.js";
+import { main as desatrelarFormacao } from "./src/implementacoes/desatrelarFormacao/MockIndex.js";
+import { main as validarCargaHoraria } from "./src/implementacoes/validarCargaHoraria/MockIndex.js";
+import { main as espelharFormacao } from "./src/implementacoes/espelharFormacao/MockIndex.js";
+
 import readline from "readline";
 
 console.log(
@@ -8,7 +11,7 @@ console.log(
 
 console.log(
   "\x1b[0m",
-  "Escolha um caso de uso há ser executado:\n1 - Imprimir certiicado\n2 - Validar carga horária\n3 - Desatrelar formação\n4 - Espelhar formação"
+  "Escolha um caso de uso há ser executado:\n1 - Validar carga horária\n2 - Desatrelar formação\n3 - Espelhar formação"
 );
 
 const input = readline.createInterface({
@@ -19,6 +22,13 @@ const input = readline.createInterface({
 input.question("\n-> ", function (resposta) {
   switch (resposta) {
     case "1":
+      desatrelarFormacao();
+      break;
+    case "2":
+      validarCargaHoraria();
+      break;
+    case "3":
+      espelharFormacao();
       break;
 
     default:
